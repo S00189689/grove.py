9556cc35bfe5c45dc85e7f5c3556512b3bc053b1
S00189689 <47177477+S00189689@users.noreply.github.com>
2019-08-16 20:37:53 +0100

    code from examples

---
+++ example1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+
+#pin 5 on grove hat
+
+import time
+from grove.grove_ryb_led_button import GroveLedButton
+
+def main():
+    ledbtn = GroveLedButton(5)
+
+    while True:
+        ledbtn.led.light(True)
+        time.sleep(1)
+
+        ledbtn.led.light(False)
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main()
+
+
